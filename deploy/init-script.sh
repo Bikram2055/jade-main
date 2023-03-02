cfd63b8f2f65916a3d66b36bd717b2ca39c068f8
Bikram2055 <subedibikram33@gmail.com>
2023-03-02 15:49:25 +0545

    change for precommit

--- deploy/init-script.sh
+++ deploy/init-script.sh
@@ -37,3 +37,4 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -f ./docker-compose-prod.yml build
 docker-compose -f ./docker-compose-prod.yml up -d
+#change for precommit
